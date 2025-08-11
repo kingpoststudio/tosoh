3ab5c6b305f720ff3a9d63f7adc50ba26b870398
tlhfckoctbcr <stu@campgrantnc.com>
2025-08-11 16:59:30 -0400

    Footer baby

---
+++ themes/tosoh2025/lib/components/Footer/Footer.svelte
@@ -0,0 +1,29 @@
+<svelte:options customElement="tosoh-footer" />
+
+<script lang="ts">
+</script>
+
+<footer>
+  <div class="wrapper"></div>
+</footer>
+
+<style lang="postcss">
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  footer {
+    background: var(--color-navy-blue);
+
+    > .wrapper {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      min-height: 20rem;
+      max-width: var(--container-8xl);
+      margin: 0 auto;
+    }
+  }
+</style>
