ec42d16828638ae403ff9907132f7db37963c372
tlhfckoctbcr <stu@campgrantnc.com>
2025-08-11 22:36:01 -0400

    Even more.

--- themes/tosoh2025/lib/main.ts
+++ themes/tosoh2025/lib/main.ts
@@ -1,2 +1,4 @@
 export { default as Header } from "./components/Header/Header.svelte";
 export { default as GridAccent } from "./components/GridAccent/GridAccent.svelte";
+
+import './styles/main.css';
