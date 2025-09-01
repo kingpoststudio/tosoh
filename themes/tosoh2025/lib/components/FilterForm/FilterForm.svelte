68e6c6f7ce60cc6894f19e0ed51c87a4bc95d2c8
Jim-Nouvakis <88037643+Jim-Nouvakis@users.noreply.github.com>
2025-09-01 11:58:21 +0300

    Refactor FilterForm component to improve form management and event handling
    - Enhanced the FilterForm component by integrating a form manager for better event handling.
    - Added support for onChange, onSubmit, and onReset callbacks to improve interactivity.
    - Implemented lifecycle management for the form manager to ensure proper cleanup on component destruction.

--- themes/tosoh2025/lib/components/FilterForm/FilterForm.svelte
+++ themes/tosoh2025/lib/components/FilterForm/FilterForm.svelte
@@ -1,8 +1,66 @@
 <script lang="ts">
-  import type { FormManagerInstance } from '../../utils/formManager';
+  import type { Snippet } from 'svelte';
+  import { onDestroy } from 'svelte';
 
-  let { type }: { type: 'change' } = $props();
+  import {
+    createFormManager,
+    type FormManagerInstance,
+    type triggerType,
+  } from '../../utils/formManager';
+
+  let {
+    trigger,
+    onChange,
+    onSubmit,
+    onReset,
+    children,
+  }: {
+    trigger: triggerType;
+    onSubmit?: (e: Event) => void;
+    onChange?: (e: Event) => void;
+    onReset?: () => void;
+    children: Snippet;
+  } = $props();
 
   let formElement: HTMLFormElement | null = $state(null);
   let formManager: FormManagerInstance | null = $state(null);
+
+  const initiateFormManager = () => {
+    if (formElement && !formManager) {
+      formManager = createFormManager(formElement, {
+        onChange: (e) => {
+          if (formElement && onChange) {
+            onChange(e);
+          }
+        },
+        onSubmit(e) {
+          if (formElement && onSubmit) {
+            onSubmit(e);
+          }
+        },
+        onReset: () => {
+          if (formElement && onReset) {
+            onReset();
+          }
+        },
+        triggerType: trigger,
+      });
+    }
+  };
+
+  $effect(() => {
+    if (formElement && !formManager) {
+      initiateFormManager();
+    }
+  });
+
+  onDestroy(() => {
+    if (formManager) {
+      formManager.destroy();
+    }
+  });
 </script>
+
+<form bind:this={formElement}>
+  {@render children()}
+</form>
