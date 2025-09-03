bf01655e9a5f1bcdea650c5cebfb2df2cadbb9ed
Jim-Nouvakis <88037643+Jim-Nouvakis@users.noreply.github.com>
2025-09-03 10:07:56 +0300

    Add Flex Columns module with customizable settings and responsive design
    - Introduced a new Flex Columns module featuring customizable column settings, including alignment, background color, and padding options.
    - Implemented dynamic fields for content types such as text, media, forms, and rich text, enhancing flexibility in layout design.
    - Added utility functions for managing field values and options, improving the overall user experience.
    - Updated HTML and JSON files to support the new module's structure and functionality, ensuring responsive design across devices.
    - Removed the previous Content Block module to streamline the codebase and avoid redundancy.

--- themes/tosoh2025/lib/utils/FieldUtils.ts
+++ themes/tosoh2025/lib/utils/FieldUtils.ts
@@ -1,4 +1,11 @@
-import { choiceField, groupField, textField, urlField } from 'hs-fieldkit';
+import {
+  booleanField,
+  choiceField,
+  groupField,
+  numberField,
+  textField,
+  urlField,
+} from 'hs-fieldkit';
 
 export const breadCrumbField = () => {
   return [
@@ -21,6 +28,7 @@ export const breadCrumbField = () => {
 
 export const sizeChoices = [
   ['auto', 'Auto'],
+  ['0', 'None'],
   ['2xs', '2X-small'],
   ['xs', 'Extra-small'],
   ['sm', 'Small'],
@@ -31,58 +39,216 @@ export const sizeChoices = [
   ['2xl', '2X-large'],
   ['3xl', '3X-large'],
   ['4xl', '4X-large'],
-  ['0', 'None'],
+  ['5xl', '5X-large'],
+  ['6xl', '6X-large'],
+  ['7xl', '7X-large'],
+  ['8xl', '8X-large'],
 ];
 
-export const constructFieldValues = (tailwindIdentifier: string) => {
-  let sizeChoicesWithPrefixes: any = [];
+export const constructFieldValues = (tailwindIdentifier: string, choices: string[][]) => {
+  let choicesWithPrefixes: any = [];
 
-  sizeChoices.map((value) => {
-    sizeChoicesWithPrefixes.push([`${tailwindIdentifier}-${value[0]}`, `${value[1]}`]);
+  choices.map((value) => {
+    choicesWithPrefixes.push([`${tailwindIdentifier}-${value[0]}`, `${value[1]}`]);
   });
 
-  return sizeChoicesWithPrefixes;
+  return choicesWithPrefixes;
 };
+
+export const paddingFields = [
+  choiceField('pt_mobile', 'Top padding (mobile)', {
+    choices: constructFieldValues('pt', sizeChoices),
+    display_width: 'half_width',
+  }),
+  choiceField('pt_desktop', 'Top padding (desktop)', {
+    choices: constructFieldValues('md:pt', sizeChoices),
+    display_width: 'half_width',
+  }),
+  choiceField('pr_mobile', 'Right padding (mobile)', {
+    choices: constructFieldValues('pr', sizeChoices),
+    display_width: 'half_width',
+  }),
+  choiceField('pr_desktop', 'Right padding (desktop)', {
+    choices: constructFieldValues('md:pr', sizeChoices),
+    display_width: 'half_width',
+  }),
+  choiceField('pb_mobile', 'Bottom padding (mobile)', {
+    choices: constructFieldValues('pb', sizeChoices),
+    display_width: 'half_width',
+  }),
+  choiceField('pb_desktop', 'Bottom padding (desktop)', {
+    choices: constructFieldValues('md:pb', sizeChoices),
+    display_width: 'half_width',
+  }),
+  choiceField('pl_mobile', 'Left padding (mobile)', {
+    choices: constructFieldValues('pl', sizeChoices),
+    display_width: 'half_width',
+  }),
+  choiceField('pl_desktop', 'Left padding (desktop)', {
+    choices: constructFieldValues('md:pl', sizeChoices),
+    display_width: 'half_width',
+  }),
+];
+
+export const alignmentChoices = [
+  ['start', 'Start'],
+  ['center', 'Center'],
+  ['end', 'End'],
+];
+
+export const textAlignmentChoices = [
+  ['left', 'Left'],
+  ['center', 'Center'],
+  ['right', 'Right'],
+];
 
 export const presetPaddingFields = [
   choiceField('pt_mobile', 'Top padding (mobile)', {
-    choices: constructFieldValues('pt'),
+    choices: constructFieldValues('pt', sizeChoices),
     default: 'pt-md',
     display_width: 'half_width',
   }),
   choiceField('pt_desktop', 'Top padding (desktop)', {
-    choices: constructFieldValues('md:pt'),
+    choices: constructFieldValues('md:pt', sizeChoices),
     default: 'md:pt-md',
     display_width: 'half_width',
   }),
   choiceField('pr_mobile', 'Right padding (mobile)', {
-    choices: constructFieldValues('pr'),
+    choices: constructFieldValues('pr', sizeChoices),
     default: 'pr-md',
     display_width: 'half_width',
   }),
   choiceField('pr_desktop', 'Right padding (desktop)', {
-    choices: constructFieldValues('md:pr'),
-    default: 'md:pr-md',
+    choices: constructFieldValues('md:pr', sizeChoices),
+    default: 'md:pr-xl',
     display_width: 'half_width',
   }),
   choiceField('pb_mobile', 'Bottom padding (mobile)', {
-    choices: constructFieldValues('pb'),
+    choices: constructFieldValues('pb', sizeChoices),
     default: 'pb-md',
     display_width: 'half_width',
   }),
   choiceField('pb_desktop', 'Bottom padding (desktop)', {
-    choices: constructFieldValues('md:pb'),
+    choices: constructFieldValues('md:pb', sizeChoices),
     default: 'md:pb-md',
     display_width: 'half_width',
   }),
   choiceField('pl_mobile', 'Left padding (mobile)', {
-    choices: constructFieldValues('pl'),
+    choices: constructFieldValues('pl', sizeChoices),
     default: 'pl-md',
     display_width: 'half_width',
   }),
   choiceField('pl_desktop', 'Left padding (desktop)', {
-    choices: constructFieldValues('md:pl'),
-    default: 'md:pl-md',
+    choices: constructFieldValues('md:pl', sizeChoices),
+    default: 'md:pl-xl',
     display_width: 'half_width',
   }),
 ];
+
+export const contentBlockRtfFeatures = [
+  'indents',
+  'charmap',
+  'advanced_emphasis',
+  'standard_emphasis',
+  'anchor',
+  'colors',
+  'block',
+  'link',
+  'cta',
+  'embed',
+  'video',
+  'hr',
+  'table',
+  'personalize',
+  'emoji',
+  'image',
+  'lists',
+  'alignment',
+  'lineheight',
+];
+
+export const linkVariantChoices = [
+  ['button', 'Button (standard)'],
+  ['button outlined', 'Button (outlined)'],
+];
+
+export const themeColorChoices = [
+  ['default', 'Black Charcoal'],
+  ['imperial-red', 'Imperial Red'],
+  ['nickel', 'Nickel'],
+  ['ghost-white', 'Ghost White'],
+  ['navy-blue', 'Navy Blue'],
+];
+
+export const offsetFields = [
+  numberField('offset_x_mobile', 'X-axis offset (mobile)', {
+    default: 0,
+    min: -100,
+    max: 100,
+    step: 1,
+    suffix: 'rem',
+    inline_help_text: 'Positive values move it to the right, negative values to the left.',
+  }),
+  numberField('offset_x_desktop', 'X-axis offset (desktop)', {
+    default: 0,
+    min: -100,
+    max: 100,
+    step: 1,
+    suffix: 'rem',
+    inline_help_text: 'Positive values move it to the right, negative values to the left.',
+  }),
+  numberField('offset_y_mobile', 'Y-axis offset (mobile)', {
+    default: 0,
+    min: -100,
+    max: 100,
+    step: 1,
+    suffix: 'rem',
+    inline_help_text: 'Positive values move it down, negative values up.',
+  }),
+  numberField('offset_y_desktop', 'Y-axis offset (desktop)', {
+    default: 0,
+    min: -100,
+    max: 100,
+    step: 1,
+    suffix: 'rem',
+    inline_help_text: 'Positive values move it down, negative values up.',
+  }),
+];
+
+//Animations
+export const animationTravelChoices = [
+  ['-2rem', 'Left/Down (large)'],
+  ['-1rem', 'Left/Down (small)'],
+  ['0', 'No travel'],
+  ['1rem', 'Right/Up (small)'],
+  ['2rem', 'Right/Up (large)'],
+];
+
+export const animationSettingsGroup = groupField('animation_settings', 'Animation settings', {
+  children: [
+    booleanField('is_animated', 'Enable animations?', {
+      inline_help_text:
+        'Enables animations when the module and/or its elements are visible within the viewport.',
+    }),
+    numberField('delay', 'Animation delay (ms)', {
+      default: 500,
+      min: 0,
+      max: 5000,
+      step: 50,
+    }),
+    numberField('duration', 'Animation duration (ms)', {
+      default: 500,
+      min: 0,
+      max: 5000,
+      step: 50,
+    }),
+    choiceField('x_travel', 'X-axis travel distance', {
+      choices: animationTravelChoices,
+      default: '0',
+    }),
+    choiceField('y_travel', 'Y-axis travel distance', {
+      choices: animationTravelChoices,
+      default: '0',
+    }),
+  ],
+});
---
+++ themes/tosoh2025/src/modules/FlexColumns.module/fields.ts
@@ -0,0 +1,333 @@
+import {
+  booleanField,
+  choiceField,
+  ctaField,
+  formField,
+  groupField,
+  imageField,
+  linkField,
+  numberField,
+  richTextField,
+  textField,
+  urlField,
+  videoField,
+} from 'hs-fieldkit';
+import {
+  alignmentChoices,
+  animationSettingsGroup,
+  constructFieldValues,
+  contentBlockRtfFeatures,
+  linkVariantChoices,
+  offsetFields,
+  paddingFields,
+  presetPaddingFields,
+  sizeChoices,
+  textAlignmentChoices,
+  themeColorChoices,
+} from '../../../lib/utils/fieldUtils';
+
+/* Column Settings */
+const columnSettingsGroup = groupField('column_settings', 'Column Settings', {
+  children: [
+    numberField('flex_factor', 'Flex factor', {
+      default: 0,
+      min: 0,
+      max: 8,
+      step: 1,
+      inline_help_text:
+        'Determines the relative width of the column compared to others. Think of it like dividing a pie - if one column has flex factor 2 and another has flex factor 1, the first column will be twice as wide as the second. <strong>If flex factor is set to 0, you will be able to set a fixed width for the column.</strong>',
+    }),
+
+    choiceField('column_size', 'Column size', {
+      choices: constructFieldValues('w', sizeChoices),
+      visibility: {
+        controlling_field_path: 'columns.column_settings.flex_factor',
+        controlling_value_regex: 0,
+        operator: 'EQUAL',
+      },
+    }),
+    choiceField('align', 'Alignment (vertical)', {
+      choices: constructFieldValues('items', alignmentChoices),
+      default: 'items-start',
+      inline_help_text: 'Controls the y-axis alignment of content within the column.',
+    }),
+    choiceField('justify', 'Justification (horizontal)', {
+      choices: constructFieldValues('justify', alignmentChoices),
+      default: 'justify-start',
+      inline_help_text: 'Controls the x-axis justification of content within the column.',
+    }),
+    choiceField('bg_color', 'Background color', {
+      choices: constructFieldValues('bg', themeColorChoices),
+      inline_help_text: 'Sets the background color for this column.',
+    }),
+    choiceField('text_color', 'Text color', {
+      choices: constructFieldValues('text', themeColorChoices),
+      inline_help_text: 'Sets the text color for this column.',
+    }),
+    choiceField('border_color', 'Border color', {
+      choices: constructFieldValues('border', themeColorChoices),
+      default: 'border-transparent',
+      inline_help_text: 'Sets the border color for this column.',
+    }),
+
+    ...paddingFields,
+    ...offsetFields,
+  ],
+});
+
+const generateFields = () => {
+  return [
+    groupField('columns', 'Columns', {
+      children: [
+        choiceField('type', 'Column type', {
+          choices: [
+            ['content', 'Content'],
+            ['media', 'Media'],
+            ['form', 'Form'],
+            ['spacer', 'Spacer'],
+            ['rtf', 'Richtext'],
+          ],
+          inline_help_text: 'Select the type of content to display in this column.',
+        }),
+
+        /* Content */
+        groupField('content', 'Content', {
+          inline_help_text:
+            'A content block provides a pre-structured layout for displaying text, images, links and more.',
+          children: [
+            choiceField('text_align', 'Text Alignment', {
+              choices: constructFieldValues('text', textAlignmentChoices),
+              default: 'text-start',
+              inline_help_text: 'Controls the alignment of content within this column.',
+            }),
+            richTextField('eyebrow', 'Eyebrow', {
+              enabled_features: contentBlockRtfFeatures,
+              inline_help_text:
+                'Optional short text above the title, often used for labeling or categorization.',
+            }),
+            richTextField('title', 'Title', {
+              enabled_features: contentBlockRtfFeatures,
+              inline_help_text: 'Main heading for this content block.',
+            }),
+            richTextField('description', 'Description', {
+              enabled_features: contentBlockRtfFeatures,
+              inline_help_text: 'Descriptive text providing more details about this content block.',
+            }),
+            booleanField('is_cta', 'Is link a CTA?', {
+              inline_help_text: 'Enable if the link should be styled as a call-to-action button.',
+            }),
+            ctaField('cta', 'CTA', {
+              visibility: {
+                controlling_field_path: 'columns.content.is_cta',
+                controlling_value_regex: 'true',
+                operator: 'EQUAL',
+              },
+              inline_help_text: 'Configure the call-to-action button for this content block.',
+            }),
+            linkField('link', 'Link', {
+              visibility: {
+                controlling_field_path: 'columns.content.is_cta',
+                controlling_value_regex: 'false',
+                operator: 'EQUAL',
+              },
+              inline_help_text: 'Add a standard link for this content block.',
+            }),
+            textField('link_label', 'Link label', {
+              default: 'Learn More',
+              visibility: {
+                controlling_field_path: 'columns.content.is_cta',
+                controlling_value_regex: 'false',
+                operator: 'EQUAL',
+              },
+              inline_help_text: 'Text to display for the link.',
+            }),
+            choiceField('link_variant', 'Link variant', {
+              choices: linkVariantChoices,
+              default: 'button',
+              visibility: {
+                controlling_field_path: 'columns.content.is_cta',
+                controlling_value_regex: 'false',
+                operator: 'EQUAL',
+              },
+              inline_help_text:
+                'Choose the style of the link. You can select between a button or a text link.',
+            }),
+          ],
+          visibility: {
+            controlling_field_path: 'columns.type',
+            controlling_value_regex: 'content',
+            operator: 'EQUAL',
+          },
+        }),
+
+        /* Media */
+        groupField('media', 'Media', {
+          inline_help_text:
+            'A media block that allows you to add a shaped image or video with a caption.',
+          children: [
+            choiceField('type', 'Media type', {
+              choices: [
+                ['image', 'Image'],
+                ['stacked_images', 'Stacked Images'],
+                ['hs_video', 'Video (HubSpot)'],
+              ],
+              default: 'image',
+              inline_help_text:
+                'Choose whether to display an image, a HiArc pattern, a HubSpot video hosted locally, or an external video URL.',
+            }),
+            imageField('image', 'Image', {
+              visibility: {
+                controlling_field_path: 'columns.media.type',
+                controlling_value_regex: 'image',
+                operator: 'EQUAL',
+              },
+              inline_help_text: 'Upload or select an image to display.',
+            }),
+
+            booleanField('is_image_contained', 'Is image contained?', {
+              inline_help_text:
+                'If enabled, the image will be contained within the column, otherwise it will fill the column width.',
+              visibility: {
+                controlling_field_path: 'columns.media.type',
+                controlling_value_regex: 'image',
+                operator: 'EQUAL',
+              },
+            }),
+            videoField('hs_video', 'Video (HubSpot)', {
+              visibility: {
+                controlling_field_path: 'columns.media.type',
+                controlling_value_regex: 'hs_video',
+                operator: 'EQUAL',
+              },
+            }),
+
+            groupField('stacked_images', 'Stacked Images', {
+              visibility: {
+                children: [imageField('image', 'Image')],
+                controlling_field_path: 'columns.media.type',
+                controlling_value_regex: 'stacked_images',
+                operator: 'EQUAL',
+              },
+              occurrence: {
+                min: 1,
+                max: 2,
+              },
+              inline_help_text: 'Upload or select an 2 images to display.',
+            }),
+          ],
+          visibility: {
+            controlling_field_path: 'columns.type',
+            controlling_value_regex: 'media',
+            operator: 'EQUAL',
+          },
+        }),
+
+        /* Form */
+        groupField('form', 'Form', {
+          inline_help_text:
+            'A form block that allows you to add a form with a title and description.',
+          children: [
+            urlField('meeting_url', 'Meeting Url', {
+              inline_help_text:
+                'Meeting link to which the user will be redirected after form submission.',
+            }),
+            richTextField('eyebrow', 'Eyebrow', {
+              enabled_features: contentBlockRtfFeatures,
+              inline_help_text: 'Optional short text above the form title.',
+            }),
+            richTextField('title', 'Title', {
+              enabled_features: contentBlockRtfFeatures,
+              inline_help_text: 'Main heading for the form.',
+            }),
+            richTextField('description', 'Description', {
+              enabled_features: contentBlockRtfFeatures,
+              inline_help_text: 'Descriptive text providing more details about the form.',
+            }),
+            formField('form', 'Form', {
+              inline_help_text: 'Select or configure the form to display in this column.',
+            }),
+          ],
+          visibility: {
+            controlling_field_path: 'columns.type',
+            controlling_value_regex: 'form',
+            operator: 'EQUAL',
+          },
+        }),
+
+        /* Richtext */
+        groupField('richtext', 'Richtext', {
+          inline_help_text:
+            'A richtext block allows you to add custom-formatted text content. This is a powerful way to present information in a flexible format, but it could be less structured than other options. NOTE: This is not a WYSIWYG editor, so you may step outside the boundaries of the design system.',
+          children: [
+            richTextField('content', 'Content', {
+              inline_help_text: 'Enter the rich text content for this block.',
+            }),
+          ],
+          visibility: {
+            controlling_field_path: 'columns.type',
+            controlling_value_regex: 'rtf',
+            operator: 'EQUAL',
+          },
+        }),
+        columnSettingsGroup,
+        animationSettingsGroup,
+      ],
+      occurrence: {
+        sorting_label_field: 'columns.type',
+      },
+    }),
+    groupField('module_settings', 'Module Settings', {
+      children: [
+        choiceField('bg_color', 'Background color', {
+          choices: themeColorChoices,
+          default: 'transparent',
+          inline_help_text: 'Sets the background color for the entire module.',
+        }),
+        choiceField('text_color', 'Text color', {
+          choices: themeColorChoices,
+          default: 'petrol',
+          inline_help_text: 'Sets the text color for the entire module.',
+        }),
+        choiceField('gap', 'Gap between columns', {
+          choices: sizeChoices,
+          default: 'md',
+          inline_help_text: 'Controls the spacing between columns.',
+        }),
+        choiceField('justify', 'Justification (horizontal)', {
+          choices: [
+            ['justify-start', 'Start'],
+            ['justify-center', 'Center'],
+            ['justify-end', 'End'],
+            ['justify-between', 'Between'],
+            ['justify-around', 'Around'],
+            ['justify-evenly', 'Evenly'],
+          ],
+          default: 'justify-center',
+          inline_help_text:
+            'Controls the x-axis justification of all content within the module or tab.',
+          visibility: {
+            controlling_field_path: `module_settings.overflow_scroll_x`,
+            controlling_value_regex: 'false',
+            operator: 'EQUAL',
+          },
+        }),
+        booleanField('overflow_scroll_x', 'Enable horizontal scrolling?', {
+          default: false,
+          inline_help_text:
+            'When enabled, columns will scroll horizontally on mobile devices instead of stacking vertically.',
+        }),
+        booleanField('is_reversed_mobile', 'Reverse columns on mobile?', {
+          inline_help_text: 'Reverses the order of columns on mobile devices.',
+          visibility: {
+            controlling_field_path: `module_settings.overflow_scroll_x`,
+            controlling_value_regex: 'false',
+            operator: 'EQUAL',
+          },
+        }),
+        ...presetPaddingFields,
+      ],
+    }),
+  ];
+};
+
+export default generateFields;
