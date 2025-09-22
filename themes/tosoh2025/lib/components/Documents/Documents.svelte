8ec05aac91cafe0b9f333b98db28d7baa81a3e73
Jim-Nouvakis <88037643+Jim-Nouvakis@users.noreply.github.com>
2025-09-22 13:50:52 +0300

    Add Documents component and related files for document management
    - Introduced a new Documents component with associated subcomponents including Card, DocumentsFilters, and SkeletonCard for enhanced document management functionality.
    - Updated main.ts to include the new Documents component.
    - Created a new kiosk-documents.html file to serve as the entry point for the document management interface.
    - Removed the obsolete SupportPortalGrid component to streamline the codebase.

---
+++ themes/tosoh2025/lib/components/Documents/Documents.svelte
@@ -0,0 +1,129 @@
+<svelte:options
+  customElement={{
+    tag: 'tosoh-documents',
+    shadow: 'none',
+  }}
+/>
+
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  import DocumentsFilters from './DocumentsFilters.svelte';
+  import ErrorCard from '../ErrorCard/ErrorCard.svelte';
+
+  import {
+    defaultItemsLimit,
+    defaultPagination,
+    PROD_TOSOH_SUPPORT_PORTAL_TABLE_ID,
+  } from '../../utils/constants';
+  import PaginationWithLimit from '../PaginationWithLimit/PaginationWithLimit.svelte';
+  import Card from './Card.svelte';
+  import SkeletonCard from './SkeletonCard.svelte';
+  import ItemsGrid from '../ItemsGrid/ItemsGrid.svelte';
+  import { fetchTableRows } from '../../services/fetchTableRows';
+
+  let availableFilters = window?.Tosoh?.SupportPortalContent?.filters
+    ? window?.Tosoh?.SupportPortalContent?.filters.split(',')
+    : [];
+
+  let searchColumnId = window?.Tosoh?.SupportPortalContent?.search
+    ? window?.Tosoh?.SupportPortalContent?.search?.hubdb_column_id
+    : '';
+
+  let title = window?.Tosoh?.SupportPortalContent?.title;
+  let description = window?.Tosoh?.SupportPortalContent?.description;
+
+  let tableRows: any = $state([]);
+  let totalItems = $state(0);
+  let hasError = $state(false);
+  let isLoading = $state(false);
+
+  const params = new URLSearchParams(window.location.search);
+  let viewAs: 'grid' | 'list' = $state((params.get('view') as 'grid' | 'list') || 'grid');
+
+  const constructFilterParams = () => {
+    const params = new URLSearchParams(window.location.search);
+    let objWithFilters: any = {};
+    const allFilters = [...availableFilters, searchColumnId];
+    //product_family, product-type, document_category, document_type
+    allFilters?.map((filter) => (objWithFilters[filter] = params?.get(filter) || ''));
+    return { ...objWithFilters };
+  };
+
+  const constructBody = () => {
+    const params = new URLSearchParams(window.location.search);
+    return {
+      tableId: PROD_TOSOH_SUPPORT_PORTAL_TABLE_ID,
+      properties:
+        'name,image,hs_path,product_family,product_type,document_type,wistia_video_url,document_url',
+      limit: parseInt(params?.get('limit') || defaultItemsLimit),
+      pagination: parseInt(params?.get('pagination') || defaultPagination),
+      offset:
+        parseInt(params?.get('limit') || defaultItemsLimit) *
+          (parseInt(params?.get('pagination') || defaultPagination) - 1) || 0,
+      filters: constructFilterParams(),
+    };
+  };
+
+  const fetchData = async () => {
+    try {
+      isLoading = true;
+      const data = await fetchTableRows(constructBody());
+      const { results, total } = data ?? { results: [], total: 0 };
+      tableRows = results;
+      totalItems = total;
+    } catch (error) {
+      hasError = true;
+    } finally {
+      isLoading = false;
+    }
+  };
+
+  const reloadData = () => {
+    hasError = false;
+    fetchData();
+  };
+
+  const handleChangeView = () => {
+    const url = new URL(window.location.href);
+    url.searchParams.set('view', viewAs === 'grid' ? 'list' : 'grid');
+    window.history.pushState({}, '', url.toString().replace(/%2C/g, ','));
+    viewAs = viewAs === 'grid' ? 'list' : 'grid';
+  };
+
+  onMount(() => {
+    fetchData();
+  });
+</script>
+
+{#if title || description}
+  <div class="max-w-max-page gap-md p-md md:pl-2xl md:pr-2xl m-auto flex flex-col">
+    {#if title}
+      <h2 class="font-bold">{title}</h2>
+    {/if}
+
+    {#if description}
+      <h6 class="text-nickel">{description}</h6>
+    {/if}
+  </div>
+{/if}
+
+<div
+  class={`p-md  md:pl-2xl md:pr-2xl gap-base max-w-max-page relative m-auto mb-32 flex w-full flex-col justify-around lg:flex-row ${title || description ? '' : 'mt-lg'}`}
+>
+  <DocumentsFilters isParentLoading={isLoading} {viewAs} {handleChangeView}></DocumentsFilters>
+  <div class="flex w-full flex-col justify-between">
+    {#if hasError}
+      <div class="p-sm">
+        <ErrorCard message="Failed to load portal items" retryCallback={reloadData} />
+        <div class="pb-sm"></div>
+      </div>
+    {:else}
+      <ItemsGrid {tableRows} {isLoading} {viewAs} {Card} {SkeletonCard}></ItemsGrid>
+
+      {#if tableRows?.length > 0}
+        <PaginationWithLimit {totalItems}></PaginationWithLimit>
+      {/if}
+    {/if}
+  </div>
+</div>
