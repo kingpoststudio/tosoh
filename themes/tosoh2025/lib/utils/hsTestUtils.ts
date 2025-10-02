1dced06361ed240e05a82463c10dddcd1bb8b73c
Jim-Nouvakis <88037643+Jim-Nouvakis@users.noreply.github.com>
2025-10-02 20:37:00 +0300

    Update carousel image styles and enhance layout consistency
    - Adjusted carousel image dimensions to improve visual consistency across modules.
    - Refined layout elements in RelatedBlogPosts.module.html for better user experience.

---
+++ themes/tosoh2025/lib/utils/hsTestUtils.ts
@@ -0,0 +1,34 @@
+// TODO: this is to be done
+
+export const constructColumnItem = (id: string, name: string, label: string, type: string) => {
+  return {
+    id,
+    name,
+    label,
+    type,
+  };
+};
+
+// {
+//     id: '6',
+//     name: 'HPLC Service',
+//     label: 'HPLC Service',
+//     type: 'option',
+//     createdAt: '2025-08-12T15:23:44.066Z',
+//     createdByUserId: 69586742,
+//     updatedAt: '2025-08-12T15:23:44.066Z',
+//     updatedByUserId: 69586742,
+//     order: 5,
+//   },
+
+// {
+//     url: 'https://landing.tosoh-diagnostics.com/hubfs/customer-support/TSE%20FSE%20CS/Category%20images/Application%20Troubleshooting.png',
+//     width: 1024,
+//     height: 1024,
+//     altText: '',
+//     fileId: 191600953778,
+//     type: 'image',
+//   },
+
+// search_terms:'string',
+//start_date:1234667
