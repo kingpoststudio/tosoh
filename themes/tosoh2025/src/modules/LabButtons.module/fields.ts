a4ea0be1c6fd51c064a58e79058532b3fac07fb8
Jim-Nouvakis <88037643+Jim-Nouvakis@users.noreply.github.com>
2025-10-06 19:25:11 +0300

    Enhance LabButtons module with new fields and improved structure
    - Added boolean fields for enabling headline and displaying footnote.
    - Introduced rich text fields for eyebrow, title, content, and footnote.
    - Updated HTML structure to conditionally render headline and footnote based on new fields.
    - Improved organization of lab button grid rows and their properties for better clarity.

--- themes/tosoh2025/src/modules/LabButtons.module/fields.ts
+++ themes/tosoh2025/src/modules/LabButtons.module/fields.ts
@@ -1,28 +1,75 @@
-import { choiceField, groupField, imageField, textField, urlField } from 'hs-fieldkit';
+import {
+  choiceField,
+  booleanField,
+  groupField,
+  imageField,
+  richTextField,
+  textField,
+} from 'hs-fieldkit';
+import { contentBlockRtfFeatures } from '../../../lib/utils/fieldUtils';
 
 const generateFields = () => {
   return [
-    groupField('lab_buttons', 'Lab Buttons', {
+    booleanField('enable_headline', 'Enable Headline', {
+      default: true,
+    }),
+    groupField('headline', 'Headline', {
       children: [
-        textField('button_label', 'Label'),
-        urlField('link', 'Link'),
-        imageField('image', 'Image', {
-          required: true,
+        richTextField('eyebrow', 'Eyebrow', {
+          enabled_features: contentBlockRtfFeatures,
         }),
-        choiceField('size', 'Size', {
-          choices: [
-            ['small', 'Small'],
-            ['medium', 'Medium'],
-            ['large', 'Large'],
+        richTextField('title', 'Title', {
+          enabled_features: contentBlockRtfFeatures,
+        }),
+        richTextField('content', 'Content', {
+          enabled_features: contentBlockRtfFeatures,
+        }),
+      ],
+      visibility: {
+        controlling_field_path: 'enable_headline',
+        controlling_value_regex: true,
+        operator: 'EQUAL',
+      },
+    }),
+    groupField('grid_rows', 'Grid Rows', {
+      children: [
+        groupField('lab_buttons', 'Lab Buttons', {
+          children: [
+            textField('button_label', 'Label'),
+            imageField('image', 'Image', {
+              required: true,
+            }),
+            choiceField('size', 'Size', {
+              choices: [
+                ['small', 'Small'],
+                ['medium', 'Medium'],
+                ['large', 'Large'],
+              ],
+              default: 'medium',
+              required: true,
+            }),
           ],
-          default: 'medium',
-          required: true,
+          occurrence: {
+            min: null,
+            max: null,
+          },
         }),
       ],
       occurrence: {
         min: null,
         max: null,
       },
+    }),
+    booleanField('display_footnote', 'Display Footnote', {
+      default: false,
+      visibility: {
+        controlling_field_path: 'display_footnote',
+        controlling_value_regex: true,
+        operator: 'EQUAL',
+      },
+    }),
+    richTextField('footnote', 'Footnote', {
+      enabled_features: contentBlockRtfFeatures,
     }),
   ];
 };
