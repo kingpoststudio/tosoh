8e8cc94b43c9f87136ae865343e43897e403c648
Jim-Nouvakis <88037643+Jim-Nouvakis@users.noreply.github.com>
2025-10-07 12:47:29 +0300

    Add exclude_event_id field to Related Events module for filtering
    - Introduced a new field 'exclude_event_id' in the Related Events module to allow users to specify an event to exclude from the related events carousel.
    - Updated the module's HTML to filter out the excluded event, enhancing the relevance of displayed related events.
    - Modified the events template to pass the current event's ID to the Related Events module for improved functionality.

--- themes/tosoh2025/src/modules/hubdb/RelatedEvents.module/fields.ts
+++ themes/tosoh2025/src/modules/hubdb/RelatedEvents.module/fields.ts
@@ -4,7 +4,10 @@ const generateFields = () => {
   return [
     textField('eyebrow', 'Eyebrow'),
     textField('title', 'Title'),
-
+    textField('exclude_event_id', 'Exclude Event ID', {
+      inline_help_text:
+        'This is the hs_id of the event to exclude from the related events carousel. If no event is provided, all events will be displayed.',
+    }),
     textField('cta_label', 'CTA Label', {
       default: 'Read More',
     }),
