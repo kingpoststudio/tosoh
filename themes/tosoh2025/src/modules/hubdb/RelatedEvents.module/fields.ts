6a40244b93c0dc87bcfff478b057de65306891a6
Jim-Nouvakis <88037643+Jim-Nouvakis@users.noreply.github.com>
2025-10-08 10:49:05 +0300

    Add 'include_by_type' field to RelatedEvents module for event filtering
    - Introduced a new field 'include_by_type' in the RelatedEvents module to allow filtering of events by type, enhancing user control over displayed content.
    - Updated module HTML to utilize the new field for filtering events, improving the relevance of displayed related events.
    - Adjusted event rendering logic to incorporate the new filtering capability, ensuring a more tailored user experience.

--- themes/tosoh2025/src/modules/hubdb/RelatedEvents.module/fields.ts
+++ themes/tosoh2025/src/modules/hubdb/RelatedEvents.module/fields.ts
@@ -8,6 +8,11 @@ const generateFields = () => {
       inline_help_text:
         'This is the hs_id of the event to exclude from the related events carousel. If no event is provided, all events will be displayed.',
     }),
+    textField('include_by_type', 'Include By Type', {
+      inline_help_text:
+        'If no event type is provided, all events will be displayed. Example types are webinar, conference and workshop. The type is the "INTERNAL_NAME" of the "event_type" column in the HubDB table.',
+    }),
+
     textField('cta_label', 'CTA Label', {
       default: 'Read More',
     }),
