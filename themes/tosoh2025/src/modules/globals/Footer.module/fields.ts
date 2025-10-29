90103ccf694fa52263e34437ceffe1fd497d153e
Jim-Nouvakis <88037643+Jim-Nouvakis@users.noreply.github.com>
2025-10-29 16:27:33 +0200

    Refactor Footer module fields and HTML for enhanced configuration management
    - Updated Footer module fields to group configurations under 'Footer Configurations' for better organization.
    - Modified HTML to utilize active configurations for dynamic rendering of logo, information, copyright, legal links, and LinkedIn, improving usability and maintainability.
    - Adjusted occurrence settings for legal links to allow for more flexible configurations.

--- themes/tosoh2025/src/modules/globals/Footer.module/fields.ts
+++ themes/tosoh2025/src/modules/globals/Footer.module/fields.ts
@@ -1,23 +1,32 @@
 import { groupField, imageField, linkField, textField } from 'hs-fieldkit';
 
 const generateFields = () => [
-  imageField('logo', 'Logo'),
-  textField('info', 'Information block', {
-    default:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.,',
-  }),
-  textField('copyright', 'Copyright', {
-    inline_help_text: 'Text to display in the copyright notice',
-    default: '© {{ year }} Tosoh Diagnostics. All rights reserved.',
-  }),
-  groupField('legal', 'Legal links', {
-    children: [textField('linkLabel', 'Link label'), linkField('link', 'Link')],
+  groupField('footer_configurations', 'Footer Configurations', {
+    children: [
+      textField('variation_name', 'Variation name'),
+      imageField('logo', 'Logo'),
+      textField('info', 'Information block', {
+        default:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.,',
+      }),
+      textField('copyright', 'Copyright', {
+        inline_help_text: 'Text to display in the copyright notice',
+        default: '© {{ year }} Tosoh Diagnostics. All rights reserved.',
+      }),
+      groupField('legal', 'Legal links', {
+        children: [textField('linkLabel', 'Link label'), linkField('link', 'Link')],
+        occurrence: {
+          min: null,
+          max: 3,
+        },
+      }),
+      linkField('linkedin', 'LinkedIn'),
+    ],
     occurrence: {
       min: null,
-      max: 3,
+      max: null,
     },
   }),
-  linkField('linkedin', 'LinkedIn'),
 ];
 
 export default generateFields;
