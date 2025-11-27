bd720c3fc97d837c25418a859f5923b6413378ef
Jim-Nouvakis <88037643+Jim-Nouvakis@users.noreply.github.com>
2025-11-27 10:06:08 +0200

    Refactor configuration settings and update header module logic
    - Renamed fields in the ConfigSettings module for clarity, changing 'selected_header_configuration' to 'selected_configuration' and updating menu override fields for consistency.
    - Enhanced the header module's HTML to conditionally set the active configuration based on the new selected configuration field, improving dynamic content rendering.
    - Adjusted the base layout template to ensure proper integration of the updated ConfigSettings module.

--- themes/tosoh2025/src/modules/ConfigSettings.module/fields.ts
+++ themes/tosoh2025/src/modules/ConfigSettings.module/fields.ts
@@ -3,13 +3,17 @@ import { configurationChoices } from '../../../lib/utils/FieldUtils';
 
 const generateFields = () => {
   return [
-    choiceField('selected_header_configuration', 'Selected header configuration', {
+    choiceField('selected_configuration', 'Selected configuration', {
+      inline_help_text:
+        'Based on this selection, the appropriate header and footer configurations will be displayed.',
       choices: configurationChoices,
+      required: true,
+      default: 'default',
     }),
-    menuField('menu_override', 'Menu override', {
+    menuField('header_menu_override', 'Header menu override', {
       inline_help_text: 'Override the main navigation menu with a custom menu.',
     }),
-    menuField('auxiliary_menu_override', 'Auxiliary menu override', {
+    menuField('header_auxiliary_menu_override', 'Header auxiliary menu override', {
       inline_help_text: 'Override the auxiliary menu in the header with a custom menu.',
     }),
     menuField('footer_menu_override', 'Footer menu override', {
