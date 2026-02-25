94d7f88d08e702cdeeec6bc60b716f179fb880e3
Jim-Nouvakis <88037643+Jim-Nouvakis@users.noreply.github.com>
2026-02-25 13:11:47 +0200

    Logout - Dynamic Config Login - Dynamic Config

--- themes/tosoh2025/src/modules/GlobalConfigSettings.module/fields.ts
+++ themes/tosoh2025/src/modules/GlobalConfigSettings.module/fields.ts
@@ -83,6 +83,59 @@ const generateFields = () => {
         }),
       ],
     }),
+
+    groupField('logout_template_config', 'Logout Template Configuration', {
+      children: [
+        textField('title', 'Title', {
+          default: 'You are signed out',
+        }),
+        textField('sign_back_in_label', 'Sign Back In Label', {
+          default: 'Sign Back In',
+        }),
+        linkField('sign_back_in_url', 'Sign Back In URL', {
+          default: {
+            url: {
+              content_id: null,
+              href: '/_hcms/mem/login',
+            },
+            open_in_new_tab: false,
+            no_follow: false,
+          },
+        }),
+      ],
+    }),
+
+    groupField('membership_login_template_config', 'Membership Login Template Configuration', {
+      children: [
+        textField('title', 'Title', {
+          default: 'Sign In',
+        }),
+        textField('description', 'Description', {
+          default: 'This Page Is Only Available To People Who Have Been Given Access.',
+        }),
+        textField('email_label', 'Email Label', {
+          default: 'Email Address',
+        }),
+        textField('password_label', 'Password Label', {
+          default: 'Password',
+        }),
+        textField('remember_me_label', 'Remember Me Label', {
+          default: 'Remember Me',
+        }),
+        textField('reset_password_text', 'Reset Password Text', {
+          default: 'Forgot Password?',
+        }),
+        textField('submit_button_text', 'Submit Button Text', {
+          default: 'Sign In',
+        }),
+        textField('having_trouble_text', 'Having Trouble Text', {
+          default: 'Having trouble?',
+        }),
+        textField('contact_admin_text', 'Contact Admin Text', {
+          default: 'Contact the admin.',
+        }),
+      ],
+    }),
   ];
 };
 
